69b53212f894a2b7668c6db15c15772c58e1063c
Fahad Farooq <thankyoufahad@gmail.com>
2021-04-05 07:56:57 +0500

    First Snapshot of Ironstep ERP which is currently being used at Star Moulding Furniture

---
+++ tmp/faillog.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Login attempts info.
+*/
+$login_faillog = array (
+  0 => 
+  array (
+    '::1' => 0,
+    'last' => '',
+  ),
+  1 => 
+  array (
+    '39.35.210.9' => 0,
+    'last' => '',
+  ),
+  2 => 
+  array (
+    '39.35.23.130' => 0,
+    'last' => '',
+  ),
+);
